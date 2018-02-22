618aed87eb771394013fc5fc6db3d810f3fc791e
leo424y <leo424y@gmail.com>
2018-02-22 23:16:15 +0800

    baet a

--- pattern/beat.rb
+++ pattern/beat.rb
@@ -1,16 +1,10 @@
 s = 1.0 / 4 #s is speed multiplier 1.0 / 8 makes crotchet 1s or 60 crotchets/minute the default bpm
 #note use of 1.0 to make the variable floating point and not integer
-dsq = 1 * s #demi-semi-quaver
-sq = 2 * s #semi-quaver
-sqd = 3 * s #semi-quaver dotted
-q = 4 * s #quaver
-qd = 6 * s #quaver dotted
-qdd = 7 * s #quaver double dotted
-c = 8 * s #crotchet
-cd = 12 * s #crotchet dotted
-cdd = 14 * s #crotchet double dotted
-m = 16 * s #minim
-md = 24 * s #minim dotted
-mdd = 28 * s #minim double dotted
-b = 32 * s #brevea
-bd = 48 * s #breve dotted
+s = 2 * s #semi-quaver
+c = 4 * s #quaver
+cs = 6 * s #quaver dotted
+o = 8 * s #crotchet
+oc = 12 * s #crotchet dotted
+oo = 16 * s #minim
+ooc = 24 * s #minim dotted
+oooo = 32 * s #brevea
