149a7955ec244aa126c8119160ccb7b5ed6931b4
Yang-Hsiang Chang <leo424y@gmail.com>
2018-02-24 13:44:32 +0800

    This simple 6 measure drum loop underpinned Outkast's 2003 hit, Hey Ya. It features a quirky half-measure (2/4) in the 4th bar of the phrase, creating an unusual resolution.
    http://www.onlinedrummer.com/drum-beats/hey-ya-outkast-beat/
    
      hat   [5, 5, 5, 5,  5, 5, 5, 5,]
      snare [0, 0, 9, 0,  0, 0, 9, 0,]
      kick  [9, 0, 0, 9,  0, 9, 0, 0,]

---
+++ learn-edm/drum-beat/HEY YA – OUTKAST – BEAT.rb
@@ -0,0 +1,40 @@
+use_bpm 118
+
+in_thread(name: :drum_machine) do
+  hat   [5, 5, 5, 5,  5, 5, 5, 5,]
+  snare [0, 0, 9, 0,  0, 0, 9, 0,]
+  kick  [9, 0, 0, 9,  0, 9, 0, 0,]
+end
+
+drum_kits = {
+  epic: {
+    hat:   :drum_cymbal_pedal,
+    snare: :drum_snare_hard,
+    kick:  :drum_heavy_kick,
+  },
+}
+current_drum_kit = drum_kits[:epic]
+
+define :use_kit do |kit_name|
+  current_drum_kit = drum_kits[kit_name]
+end
+
+define :run_pattern do |name, pattern|
+  live_loop name do
+    pattern.each do |p|
+      sample current_drum_kit[name], amp: p/9.0
+      sleep 0.5
+    end
+  end
+end
+
+define :hat do |pattern|
+  run_pattern :hat, pattern
+end
+define :kick do |pattern|
+  run_pattern :kick, pattern
+end
+
+define :snare do |pattern|
+  run_pattern :snare, pattern
+end
