599c1885779e2a6d44081d4398f32b67626ecd45
Yang-Hsiang Chang <leo424y@gmail.com>
2018-02-24 16:19:41 +0800

    WIDE AWAKE – KATY PERRY

--- learn-edm/drum-beat/WIDE AWAKE – KATY PERRY.rb
+++ learn-edm/drum-beat/WIDE AWAKE – KATY PERRY.rb
@@ -2,13 +2,12 @@ def says(x) x.delete(' ').split('').map{|s| s.to_i} end
 use_bpm 95
 
 in_thread(name: :drum_machine) do
-  hat   says '5555 5555 5555 5555'
-  snare says '0090 0900 0090 0900'
-  kick  says '9909 9000 9909 9000'
+  hat   says '5050 5050 5050 5050'
+  snare says '0090 0090 0090 0090'
+  kick  says '9000 0000 9000 0900'
 end
 
 drum_kits = {
-  hat2: :drum_cymbal_soft,
   hat: :drum_cymbal_pedal,
   snare: :drum_snare_hard,
   kick:  :drum_tom_lo_hard,
